ea565951401b84474e22d8ead4ab070ee70196e2
adil khhursheed <adil.khursheed@arbisoft.com>
2023-08-30 15:32:24 +0500

    update items

--- items.py
+++ items.py
@@ -1,15 +1,12 @@
 from scrapy.item import Item, Field
-from scrapy.loader import ItemLoader
-from itemloaders.processors import TakeFirst, MapCompose
-from w3lib.html import remove_tags
 
 
 class ProductItem(Item):
-    retailer_sku = Field(input_processors=MapCompose(remove_tags), output_processors=TakeFirst)
+    retailer_sku = Field()
     trail = Field()
-    brand = Field(input_processors=MapCompose(remove_tags), output_processors=TakeFirst)
+    brand = Field()
     url = Field()
-    name = Field(input_processors=MapCompose(remove_tags), output_processors=TakeFirst)
+    name = Field()
     description = Field()
     product_images = Field()
     category = Field()
